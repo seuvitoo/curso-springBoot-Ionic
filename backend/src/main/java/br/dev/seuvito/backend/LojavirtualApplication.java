354e77d932e30e23a6cb9df71fe25c40a5f42a49
Victor Santos <vy.santos@live.com>
2021-07-29 19:08:51 -0300

    first commit

---
+++ backend/src/main/java/br/dev/seuvito/backend/LojavirtualApplication.java
@@ -0,0 +1,13 @@
+package br.dev.seuvito.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualApplication.class, args);
+	}
+
+}
