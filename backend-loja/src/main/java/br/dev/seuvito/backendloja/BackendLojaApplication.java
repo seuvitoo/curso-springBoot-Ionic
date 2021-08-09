cdda06e30c72f170d46decc96756e43a1507de65
Victor Santos <vy.santos@live.com>
2021-08-09 14:51:37 -0300

    [FEATURE] fazendo relacionamento PRODUTO vs CATEGORIA

--- backend-loja/src/main/java/br/dev/seuvito/backendloja/BackendLojaApplication.java
+++ backend-loja/src/main/java/br/dev/seuvito/backendloja/BackendLojaApplication.java
@@ -1,7 +1,9 @@
 package br.dev.seuvito.backendloja;
 
 import br.dev.seuvito.backendloja.entities.Categoria;
+import br.dev.seuvito.backendloja.entities.Produto;
 import br.dev.seuvito.backendloja.repositories.CategoriaRepository;
+import br.dev.seuvito.backendloja.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,20 +15,35 @@ import java.util.Arrays;
 @SpringBootApplication
 public class BackendLojaApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendLojaApplication.class, args);
-	}
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLojaApplication.class, args);
+    }
 
-		Categoria cat1 = new Categoria(null, "Informatica");
-		Categoria cat2 = new Categoria(null, "Escritorio");
+    @Override
+    public void run(String... args) throws Exception {
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Escritorio");
 
-	}
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+    }
 }
---
+++ backend-loja/src/main/java/br/dev/seuvito/backendloja/repositories/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package br.dev.seuvito.backendloja.repositories;
+
+import br.dev.seuvito.backendloja.entities.Categoria;
+import br.dev.seuvito.backendloja.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+
+}
+
+
