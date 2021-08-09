89519e35eb2b47a0a2bc641774e7da4507d4749e
Victor Santos <vy.santos@live.com>
2021-08-09 14:25:34 -0300

    [FEATURE] realizando auto insert h2

--- backend-loja/src/main/java/br/dev/seuvito/backendloja/BackendLojaApplication.java
+++ backend-loja/src/main/java/br/dev/seuvito/backendloja/BackendLojaApplication.java
@@ -1,13 +1,32 @@
 package br.dev.seuvito.backendloja;
 
+import br.dev.seuvito.backendloja.entities.Categoria;
+import br.dev.seuvito.backendloja.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BackendLojaApplication {
+public class BackendLojaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendLojaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
