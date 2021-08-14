d1623e650422be986847aef3233c261b04895722
Victor Santos <vy.santos@live.com>
2021-08-14 10:44:21 -0300

    [FEATURE] adicionando entidades estado e cidade

---
+++ backend-loja/src/main/java/br/dev/seuvito/backendloja/entities/Cidade.java
@@ -0,0 +1,63 @@
+package br.dev.seuvito.backendloja.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Cidade implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String nome;
+
+    private Estado estado;
+
+    public Cidade() {
+    }
+
+    public Cidade(Integer id, String nome, Estado estado) {
+        this.id = id;
+        this.nome = nome;
+        this.estado = estado;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cidade cidade = (Cidade) o;
+        return Objects.equals(id, cidade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
