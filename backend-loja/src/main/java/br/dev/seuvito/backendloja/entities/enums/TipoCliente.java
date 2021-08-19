88001f42c0f80a9e7f5ab66e225395451b62136a
Victor Santos <vy.santos@live.com>
2021-08-19 19:10:02 -0300

    [FEATURE] add cidade, tipo cliente, cliente

---
+++ backend-loja/src/main/java/br/dev/seuvito/backendloja/entities/enums/TipoCliente.java
@@ -0,0 +1,35 @@
+package br.dev.seuvito.backendloja.entities.enums;
+
+public enum TipoCliente {
+
+    PESSOAFISICA(1, "Pessoa Fisica"),
+    PESSOAJURIDICA(2, "Pessoa Juridica");
+
+    private int cod;
+    private String descricao;
+
+    private TipoCliente(int cod, String descricao) {
+        this.cod = cod;
+        this.descricao = descricao;
+    }
+
+    public static TipoCliente toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+        for (TipoCliente x : TipoCliente.values()) {
+            if (cod.equals(x.getCod())) {
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Id invalid: " + cod);
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
