01b24686565b0601f7d4385ac8df56343231e5c0
Victor Santos <vy.santos@live.com>
2022-06-28 18:24:13 -0300

    listando todas as categorias

--- backend-loja/src/main/java/br/dev/seuvito/backendloja/services/CategoriaService.java
+++ backend-loja/src/main/java/br/dev/seuvito/backendloja/services/CategoriaService.java
@@ -3,11 +3,11 @@ package br.dev.seuvito.backendloja.services;
 import br.dev.seuvito.backendloja.entities.Categoria;
 import br.dev.seuvito.backendloja.repositories.CategoriaRepository;
 import br.dev.seuvito.backendloja.services.execeptions.DataIntegratyException;
-import javassist.tools.rmi.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,7 +16,7 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria find(Integer id) throws ObjectNotFoundException {
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
         return obj.orElseThrow(() -> new br.dev.seuvito.backendloja.services.execeptions.ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
@@ -26,12 +26,12 @@ public class CategoriaService {
         return repo.save(obj);
     }
 
-    public Categoria update(Categoria obj) throws ObjectNotFoundException {
+    public Categoria update(Categoria obj) {
         find(obj.getId());
         return repo.save(obj);
     }
 
-    public void delete(Integer id) throws ObjectNotFoundException {
+    public void delete(Integer id) {
         find(id);
         try {
             repo.deleteById(id);
@@ -40,5 +40,9 @@ public class CategoriaService {
             throw new DataIntegratyException("você nao pode excluir uma categoria com produtos");
 
         }
+    }
+
+    public List<Categoria> findAll() {
+        return repo.findAll();
     }
 }
